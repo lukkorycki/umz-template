7116d7943a77f6fb2ca2186a366333ac32a259ec
lukkorycki <36795631+lukkorycki@users.noreply.github.com>
2018-05-13 21:30:50 +0200

    Update evaluate.py

--- zajecia3/zadanie1/evaluate.py
+++ zajecia3/zadanie1/evaluate.py
@@ -16,14 +16,17 @@ def get_sensivity(test, predictions_list):
 
 
 def get_specifity(test, predictions_list):
-    # TO TRZEBA NAPISAĆ (przekleić z get_sensivity i zmienić jedną linijkę zgodnie z
-    # definicją specifity
-    pass
+    actually_ham = sum([1 for x in test if not x.is_spam])
+    TN = len([1 for x, y in zip(test, predictions_list)
+              if (y == False and not x.is_spam)])
+    return TN / actually_ham
 
 
 def get_precision(test, predictions_list):
-    # JW
-    pass
+    all_positives = sum(predictions_list)
+    TP = len([1 for x, y in zip(test, predictions_list)
+              if (y == True and x.is_spam)])
+    return TP / all_positives
 
 
 def get_fmeas(test, predictions_list):
